3a5c3e0bf28ce88ece97d4c205d0bf9c24d1c21b
IDAETeam <105024766+IDAETeam@users.noreply.github.com>
2022-09-14 12:10:10 -0400

    Delete iv2 directory

--- iv2/equalsizes.m
+++
@@ -1,6 +0,0 @@
-function    err = equalsizes(p);
-
-global mM pL
-
-err                             = norm(sum(mM(:)>p(1)) - pL);
-
--- iv2/g0f.m
+++
@@ -1,5 +0,0 @@
-function    out                 = g0f;
-
-% To return figure number (instead of figure info in cell array: 2014b or later);
-out                             = double(gcf);
-return;
