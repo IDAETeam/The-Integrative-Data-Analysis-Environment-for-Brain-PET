9b45d9a2530bee4adf0038beb3705e2de1c44d42
IDAETeam <IDAETEAM@outlook.com>
2023-02-28 18:55:38 -0500

    Remove unprepared iv2_MUSE

--- iv2_MUSE/IDAEHeaderColor4Item.m
+++
@@ -1,9 +0,0 @@
-function ret = IDAEHeaderColor4Item()
-
-% IDAEHeaderColor:	Return color matrix for IDAE GUI header
-%
-%		usage:	<Color matrix> = IDAEHeaderColor4Item();
-%
-%
-	ret = iv2_bgcs(6);
-end
